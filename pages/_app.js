66b0a383b6c8f5dcec29d6e29d885ca43a7f2d64
Rajiv Kulkarni <rajiv1628@gmail.com>
2022-04-06 00:48:48 +0530

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import { RobinhoodProvider } from '../context/RobinhoodContext'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <MoralisProvider
+      // This should be in a .env file
+      serverUrl='https://qrsoxotgbwqc.usemoralis.com:2053/server'
+      appId='eqARtyVwkQUGEbRvD44MpeMc293fLjkTop7kjJiT'
+    >
+      <RobinhoodProvider>
+        <Component {...pageProps} />
+      </RobinhoodProvider>
+    </MoralisProvider>
+  )
+}
+
+export default MyApp
